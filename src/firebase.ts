cead14ffe5ef618b56ceafa6c78d7b80269b4e22
Somesh Rathore <somesh.topports@gmail.com>
2025-12-08 03:16:16 +0530

    fix: hardcode firebase config to resolve deployment issue

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDv1SZT1V8iVdKtDDWKENU3-TK0MgPSyi0",
+  authDomain: "streetj.firebaseapp.com",
+  projectId: "streetj",
+  storageBucket: "streetj.firebasestorage.app",
+  messagingSenderId: "868126532076",
+  appId: "1:868126532076:web:2c690e1c032235bc2a60c9",
+  measurementId: "G-5DXCGLGZDH"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
